b69991f1a75a4d3c6cd95a9458549898a5f28631
Caleb Kilmer <ckilmer2026@gmail.com>
2025-01-11 15:25:05 -0500

    Coral Fixes #8

---
+++ src/main/java/frc/robot/subsystems/Coral.java
@@ -0,0 +1,25 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.drive.RobotDriveBase.MotorType;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Coral extends SubsystemBase {
+
+//Define Neo
+SparkMax coralNeo = new SparkMax(1, MotorType.kBrushless);
+
+  //Creates a new Coral.
+    private final ShuffleboardTab coralTab = Shuffleboard.getTab("Coral");
+  public Coral() {
+    System.out.println("+++++ Starting Coral Constructor +++++");
+    System.out.println("----- Ending Coral Constructor -----");
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
+
